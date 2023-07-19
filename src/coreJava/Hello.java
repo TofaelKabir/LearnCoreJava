a8dc9cd2374fbfb37f98cfbe7a98499796ca76ad
TofaelKabir <tofaelkabir@gmail.com>
2023-07-19 10:59:41 -0400

    How to create Java project, package, class

---
+++ src/coreJava/Hello.java
@@ -0,0 +1,10 @@
+package coreJava;
+
+// we can create more than one class inside a package
+// we can create more than one package inside the src file [src is called source file]
+public class Hello {
+	// variable, constructor, methods are before main method
+	public static void main(String[] args) {
+		System.out.println("Let's learn Java with Fun!");
+	}
+}
