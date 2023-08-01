d6c0597cec012140e1ea1ae77e889cc5a68b6dbc
TofaelKabir <tofaelkabir@gmail.com>
2023-08-01 11:17:21 -0400

    print vs println, indentation, concatenation

---
+++ src/lec03_java_basic/B_use_of_indentation.java
@@ -0,0 +1,22 @@
+package lec03_java_basic;
+
+public class B_use_of_indentation {
+
+	public static void main(String[] args) {
+		System.out.println("All about Indentation");
+		
+	}
+
+}
+
+// Indentation: A new Java programmer must be a master in the concept of "indentation" in Java. 
+// Indentation is used to make our code readable to other users, easier to edit, 
+// it display how the curly braces match up and show the logic of the program in an organized fashion. 
+// The best way is, to put the cursor at the end of last line of code and press Enter
+// we learn indentation, also how to close classes when they are opened multiple or more
+
+
+// how to close the classes already open? What kind of options are present?
+// Right click on selected class -- one of them from the header and choose from below
+// close, close all, close others, close to left, close to right, detach etc
+// you choose what needed
---
+++ src/lec03_java_basic/NextClass.java
@@ -0,0 +1,10 @@
+package lec03_java_basic;
+
+public class NextClass {
+	// A class contain what?
+	// First variable
+	// Second Constructor
+	// Third Method
+	// Finally main method
+
+}
